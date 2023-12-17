f5b88e01bfcb6f3322f5b1e9c8c860e8bda20deb
Thomas M <massonthomas1@gmail.com>
2023-12-17 15:13:07 +0100

    Ajout variables et mixins scss dans les fichiers scss des composants

--- src/components/atoms/CardImage/index.jsx
+++ src/components/atoms/CardImage/index.jsx
@@ -1,7 +1,7 @@
 export default function CardImage({ title, cover }) {
     return (
         <>
-            <img src={cover} alt={title} className="author-card__pic" />
+            <img src={cover} alt={title} className="card__pic" />
         </>
     )
 }
